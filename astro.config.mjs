c0cf53e1ef408e77b0020a23fd1f63663ab9cd04
jeromemouneyraclotterywest <jerome.mouneyrac@lotterywest.wa.gov.au>
2025-03-25 11:03:37 +0800

    6 hours later...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrogrants.netlify.app",
-  integrations: [preact()]
+  site: "https://astrolotterywest.netlify.app",
+  integrations: [preact(), icon()]
 });

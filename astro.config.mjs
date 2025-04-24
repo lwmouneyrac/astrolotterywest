441c3d8df5efc8d9a61d1696a5ac18bd1c9a2efe
jeromemouneyraclotterywest <jerome.mouneyrac@lotterywest.wa.gov.au>
2025-04-24 11:50:14 +0800

    Add tailwind and start tailwindising

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import preact from "@astrojs/preact";
 
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrolotterywest.netlify.app",
-  integrations: [preact(), icon()]
+  integrations: [preact(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                background: '#fff',
+                primary: '#0073bc',
+                secondary: '#2c1b48',
+                accent: '#ff9776',
+            },
+
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+            lineHeight: {
+                normal: '1.4',
+            },
+
+            fontSize: {
+                base: '16px',
+                h1: '2.5rem',
+                small: '0.8em',
+                nav: '100%',
+            },
+
+            boxSizing: {
+                border: 'border-box',
+            },
+        },
+    },
+};
